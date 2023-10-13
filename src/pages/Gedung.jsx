6c376de804224bd9ca71e746bae775dada6657f7
nougatlolipop <august.ivan20@gmail.com>
2023-10-13 14:42:05 +0700

    modify gedung looks

--- src/pages/Gedung.jsx
+++ src/pages/Gedung.jsx
@@ -20,12 +20,16 @@ function Gedung() {
   };
 
   return (
-    <div className="flex h-screen items-center justify-center">
+    <div className="flex h-screen items-center justify-center bg-indigo-500">
+      <div className="rounded-tl-full bg-gradient-to-br from-white opacity-30 w-40 h-40 absolute right-0 bottom-0"></div>
+      <div className="rounded-tl-full bg-gradient-to-br from-white opacity-10 w-80 h-80 absolute right-0 bottom-0"></div>
+      <div className="rounded-br-full bg-gradient-to-tl from-white opacity-50 w-60 h-60 absolute left-0 top-0"></div>
+
       <div className="w-1/2 mx-auto">
         <div className="grid grid-cols-4 gap-4">
           {gedung.map((data, index) => (
             <Link
-              className="p-4 bg-indigo-500 text-white items-center justify-center rounded-lg"
+              className="p-4 bg-white text-indigo-500 items-center justify-center rounded-lg text-center"
               key={index}
               to={`/qr/${data._id}`}
             >
